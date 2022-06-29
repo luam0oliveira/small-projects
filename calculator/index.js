fe91fe707e5ec4b8001fccc4d757fa29adece783
Luam Alves de Oliveira <Luamoliveira.d@gmail.com>
2022-06-28 21:01:51 -0400

    calculator project finished

--- calculator/index.js
+++ calculator/index.js
@@ -1,34 +1,115 @@
 const numbers = document.querySelectorAll('button')
 const visor = document.getElementById('visor')
+let num1;
+let num2;
+let result;
+let op;
 const keys = {
-  "1"(){console.log(visor.value = "1")} ,
-  "2"(){},
-  "3"(){},
-  "4"(){},
-  "5"(){},
-  "6"(){},
-  "7"(){},
-  "8"(){},
-  "9"(){},
-  "0"(){},
-  "-"(){},
-  "/"(){},
-  "Enter"(){},
-  "+"(){},
-  "*"(){},
-  "."(){},
-  ","(){},
+  "1"(){addNumber(1)} ,
+  "2"(){addNumber(2)},
+  "3"(){addNumber(3)},
+  "4"(){addNumber(4)},
+  "5"(){addNumber(5)},
+  "6"(){addNumber(6)},
+  "7"(){addNumber(7)},
+  "8"(){addNumber(8)},
+  "9"(){addNumber(9)},
+  "0"(){addNumber(0)},
+  "-"(){takeNumber(); minus()},
+  "/"(){takeNumber(); dividedBy()},
+  "Enter"(){takeNumber(); setResult(op)},
+  "+"(){takeNumber(); add()},
+  "*"(){takeNumber(); times()},
+  "."(){addDot()},
+  ","(){addDot()},
+  "Backspace"(){deleteLast()},
+  "Delete"(){cleanVisor()}
 }
 
-
-
 document.addEventListener('keydown', event => {
-  if(event.key in Object.keys(keys)){
-    keys[event.key]()  
+  if(keys.hasOwnProperty(event.key)){
+    if(result){
+      result=null
+      cleanVisor()
+    }
+    keys[event.key]()
   }
+})
+
+numbers.forEach(value => {
+  value.addEventListener('click', event => {
+    if(result){
+      result=null
+      cleanVisor()
+    }
+    keys[event.target.dataset.button]()
+  })
 })
 
 const cleanVisor = () => {
   visor.value = ''
 }
 
+const addNumber = (number) => {
+  visor.value += number
+}
+
+const addDot = () => {
+  if(visor.value.search(/\./)==-1){
+    visor.value += '.'
+  }
+}
+
+const deleteLast = () => {
+  visor.value =  visor.value.slice(0, -1) ;
+}
+
+const minus = () => {
+  op = '-'
+}
+
+const add = () => {
+  op = '+'
+}
+
+const times = () =>{
+  op = '*'
+}
+
+const dividedBy = () => {
+  op = '/'
+}
+
+const setResult = (op) => {
+  switch(op) {
+    case '-':
+      result = (num1 - num2)
+      break;
+    case '+':
+      result = (num1 + num2)
+      break;
+    case '*':
+      result = (num1 * num2)
+      break;
+    case '/':
+      result = (num1 / num2)
+      break;
+  }
+  visor.value = result; 
+  cleanVariables()
+}
+
+const takeNumber = () => {
+  if(num1==null){
+    num1 = Number(visor.value)
+    cleanVisor()
+  }else if(num2==null){
+    num2 = Number(visor.value)
+  }
+}
+
+const cleanVariables = () => {
+  op=null
+  num1 = null
+  num2 = null
+}
